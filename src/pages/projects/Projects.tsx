90dcb14dadb66cd2da43343e685af1cb1b231fb6
Sam H <samuelh@missionreadyhq.com>
2025-09-11 14:14:21 +1200

    style: Enhance typography and layout in Projects component; update font families and clean up styles

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -4,14 +4,12 @@ import styles from "./Projects.module.css";
 const Projects = () => {
   return (
     <div className={styles.pageWrapper}>
-      <div className={styles.pageContentContainer}>
-        <div className={styles.navLocation}>
-          <Navigation />
-        </div>
-        <div className={styles.content}>
-          <h1>Projects</h1>
-          <p>Coming Soon!</p>
-        </div>
+      <div className={styles.navLocation}>
+        <Navigation />
+      </div>
+      <div className={styles.content}>
+        <h1>Projects</h1>
+        <p>Coming Soon!</p>
       </div>
     </div>
   );
