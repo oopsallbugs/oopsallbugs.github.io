319713d19d7826c592f3ce66ac38280a28eb4a5b
Sam H <oopsallbugs@gmail.com>
2025-09-17 12:57:12 +1200

    update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -28,5 +28,5 @@ export default defineConfig({
 
   // If you are deploying to https://<USERNAME>.github.io/, or to a custom domain through GitHub Pages (eg. www.example.com), set base to '/'.
   // Alternatively, you can remove base from the configuration, as it defaults to '/'.
-  base: "/",
+  base: "/oopsallbugs.github.io/",
 });
